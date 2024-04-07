fe768ebeab9ef058c50e1baaad88032e343129ce
Preben <587726@stud.hvl.no>
2024-04-07 17:32:10 +0200

    Hash-class complete.

--- src/main/java/no/hvl/dat110/util/Hash.java
+++ src/main/java/no/hvl/dat110/util/Hash.java
@@ -19,39 +19,72 @@ public class Hash {
 		BigInteger hashint = null;
 		
 		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		
-		// we use MD5 with 128 bits digest
-		
-		// compute the hash of the input 'entity'
-		
-		// convert the hash into hex format
-		
-		// convert the hex into BigInteger
+
+		try{
+
+			// we use MD5 with 128 bits digest
+			MessageDigest md = MessageDigest.getInstance("MD5");
+
+			// compute the hash of the input 'entity'
+			byte[] messageDigest = md.digest(entity.getBytes());
+
+			// convert the hash into hex format
+			String hex = toHex(messageDigest);
+
+			// convert the hex into BigInteger
+			hashint = new BigInteger(hex, 16);
+
+		}catch(NoSuchAlgorithmException e){
+
+			e.printStackTrace();
+		}
 		
 		// return the BigInteger
-		
 		return hashint;
 	}
 	
 	public static BigInteger addressSize() {
 		
 		// Task: compute the address size of MD5
-		
-		// compute the number of bits = bitSize()
-		
-		// compute the address size = 2 ^ number of bits
-		
-		// return the address size
-		
+
+		try{
+
+			// compute the number of bits = bitSize()
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			int length = md.getDigestLength();
+
+			// compute the address size = 2 ^ number of bits
+			int bits  = length * 8;
+			BigInteger as = new BigInteger("2");
+			as = as.pow(bits);
+
+			// return the address size
+			return as;
+
+		}catch (NoSuchAlgorithmException e){
+
+			e.printStackTrace();
+		}
+
 		return null;
 	}
 	
 	public static int bitSize() {
 		
 		int digestlen = 0;
-		
-		// find the digest length
-		
+
+		try{
+
+			// find the digest length
+			MessageDigest md =  MessageDigest.getInstance("MD5");
+
+			digestlen = md.getDigestLength();
+
+		}catch(NoSuchAlgorithmException e){
+
+			e.printStackTrace();
+		}
+
 		return digestlen*8;
 	}
 	
