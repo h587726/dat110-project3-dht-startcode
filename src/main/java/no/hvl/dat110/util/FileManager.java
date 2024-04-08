203aa1365bc5e3363ba1c3f4a60531deb8d52678
Preben <587726@stud.hvl.no>
2024-04-08 17:46:56 +0200

    requestActiveNodeForFile complete.

--- src/main/java/no/hvl/dat110/util/FileManager.java
+++ src/main/java/no/hvl/dat110/util/FileManager.java
@@ -129,14 +129,20 @@ public class FileManager {
 		// Task: Given a filename, find all the peers that hold a copy of this file
 		
 		// generate the N replicas from the filename by calling createReplicaFiles()
+		createReplicaFiles();
 		
 		// iterate over the replicas of the file
-		
-		// for each replica, do findSuccessor(replica) that returns successor s.
-		
-		// get the metadata (Message) of the replica from the successor (i.e., active peer) of the file
-		
-		// save the metadata in the set activeNodesforFile.
+		for (int i = 0; i < replicafiles.length; i++) {
+
+			// for each replica, do findSuccessor(replica) that returns successor s.
+			NodeInterface nodeInterface = chordnode.findSuccessor(replicafiles[i]);
+
+			// get the metadata (Message) of the replica from the successor (i.e., active peer) of the file
+			Message message = nodeInterface.getFilesMetadata(replicafiles[i]);
+
+			// save the metadata in the set activeNodesforFile.
+			activeNodesforFile.add(message);
+		}
 		
 		return activeNodesforFile;
 	}
